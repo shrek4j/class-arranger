b9f041bedc9f5511de04fb2980bb9b5f511b04b7
shrek4j <scxtjwgy2005@163.com>
2016-04-15 19:13:33 +0800

    logout

--- Application/Home/Controller/OperatorController.class.php
+++ Application/Home/Controller/OperatorController.class.php
@@ -2,13 +2,15 @@
 namespace Home\Controller;
 use Think\Controller;
 class OperatorController extends Controller {
-    // entrance:   http://localhost/index.php/Home/User/login
+    // entrance:   http://localhost/index.php/Home/Operator/login
     public function login(){
         $this->display();
     }
     
     public function logout(){
-        
+        session('operatorId', null);
+        session('instId', null);
+        $this->success('退出成功', '/index.php/Home/Operator/login',2);
     }
     
     public function doLogin($loginname,$password){
@@ -17,7 +19,9 @@ class OperatorController extends Controller {
         if(!empty($result) && count($result) == 1){
             session('operatorId',$result[0]['operator_id']);
             session('instId',$result[0]['inst_id']);
-            $this->success('登陆成功', '/index.php/Home/Class/classmain',1);
+            session('operatorName',$result[0]['user_name']);
+            session('role',"管理员");
+            $this->success('登陆成功', '/index.php/Home/Class/classmain',2);
         }else{
             $this->error('登陆失败');
         }    
