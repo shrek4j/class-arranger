bbd0c8062b972c70636d95163819b32d3e9a71e6
shrek4j <scxtjwgy2005@163.com>
2016-05-05 09:56:20 +0800

    20160505

--- Application/Home/Controller/ClassController.class.php
+++ Application/Home/Controller/ClassController.class.php
@@ -3,14 +3,50 @@ namespace Home\Controller;
 use Think\Controller;
 class ClassController extends Controller {
 
-    public function addClass($classname,$classtype,$teacher,$students,$classroom,
-                            $startdate,$enddate,$week,$starttime,$endtime,$remark){
-        //insert classoa_class
+    public function addClass($classname="",$classtypeId,$teacherId,$classroomId,$studentIds="1|2|3",$startDate="",$endDate="",$time="1|9:00-11:00;3|9:00-11:00",$tuition,$remark){
 
-        //insert classoa_class_detail
+        //inst_Id
+        $tId = session('tId');
 
-        //insert classoa_class_detail_student_rela
+        //handle time format
+        $classTimeArr = array("","","","","","","");
+        $timeArr = explode(';',$time);
+        for($x=0;$x<count($timeArr);$x++) {
+            $tuple = explode('|',$timeArr[$x]);
+            $classTimeArr[$tuple[0]-1] = $tuple[1];
+        }
 
+        //translate
+        $start = strtotime($startDate);
+        $end = strtotime($endDate);
+        $oneDay = 24*3600;
+
+        //students
+        $students = explode('|',$studentIds);
+
+        //save class
+        $class = new \Home\Model\ClassModel(); 
+        $classId = $class->saveClass($className,$classtypeId,$remark,$tId);
+
+        while($start < $end){
+            $dayOfWeek = date('w',$start);//get the dayOfWeek of this timestamp
+            if($classTimeArr[$dayOfWeek-1] != ""){//has class on that day
+                $date = date("Y-m-d", $start) ;
+                $month = intval(explode('-',$date)[1]);
+                $times = explode('-',$classTimeArr[$dayOfWeek-1]);
+                $startTime = $times[0];
+                $endTime = $times[1];
+                //save class detail
+                $classDetailId = $class->saveClassDetail($date,$month,$dayOfWeek,$startTime,$endTime,$teacherId,$classroomId,$classId,$tuition,$tId);
+
+                //save students that join this class
+                for($i;$i<count($students);$i++){
+                    $class->saveClassDetailAndStudentRela($classDetailId,$classId,$students[i],$tuition,$tId);
+                }
+            }
+            
+            $start += $oneDay;//check next day
+        }
 
     }
 
--- Application/Home/Model/ClassModel.class.php
+++ Application/Home/Model/ClassModel.class.php
@@ -4,7 +4,30 @@ use Think\Model;
 class ClassModel extends Model {
     protected $connection = 'DB_CONFIG1';//调用配置文件中的数据库配置1
     protected $autoCheckFields =false;//模型和数据表无需一一对应
+
+    public function saveClass($className,$classtypeId,$remark,$tId){
+        $sql = "insert into classoa_class(class_name,class_type_id,remark,inst_id) values('".$className."',".$classtypeId.",'".$remark."',".$tId.")";
+        $this->execute($sql);
+        $queryIdSql = "SELECT @@IDENTITY as class_id";
+        return $this->query($queryIdSql);
+    }
     
+    public function saveClassDetail($date,$month,$dayOfWeek,$startTime,$endTime,$teacherId,$classroomId,$classId,$tuition,$tId){
+        $sql = "insert into classoa_class_detail(date,month,day_of_week,start_time,end_time,teacher_id,classroom_id,class_id,tuition_per_class,inst_id) values('".$date."',".$month.",".$dayOfWeek.",'".$startTime."','".$endTime."',".$teacherId.",".$classroomId.",".$classId.",".$tuition.",".$tId.")";
+        $this->execute($sql);
+        $queryIdSql = "SELECT @@IDENTITY as class_detail_id";
+        return $this->query($queryIdSql);
+    }
+
+    public function saveClassDetailAndStudentRela($classDetailId,$classId,$studentId,$tuition,$tId){
+        $sql = "insert into classoa_class_detail_student_rela(class_detail_id,class_id,student_id,tuition_per_class,is_absent,inst_id) values(".$classDetailId.",".$classId.",".$studentId.",".$tuitionPerStud.",".$tId.")";
+        $this->execute($sql);
+    }
+    
+
+    ////////////////////////////////////
+    ////////////classtype///////////////
+    ////////////////////////////////////
     public function saveClasstype($classtype,$instId){
         $sql = "insert into classoa_classtype(name,inst_id) values('".$classtype."',".$instId.")";
         return $this->execute($sql);
