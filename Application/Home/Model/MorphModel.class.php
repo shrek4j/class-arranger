5742fb431cc5a1f95a591ff4f34e5110ee33e53e
shrek4j <scxtjwgy2005@163.com>
2017-04-26 16:49:49 +0800

    echart

--- Application/Home/Model/MorphModel.class.php
+++ Application/Home/Model/MorphModel.class.php
@@ -13,7 +13,7 @@ class MorphModel extends Model {
     }
 
     public function showSimilarWords($wId){
-        $sql = "SELECT ww.word,ww.translation,ww.freq_level FROM wiki_word_rela wwr LEFT JOIN wiki_word ww ON wwr.similar_word_id=ww.id WHERE wwr.word_id=%d order by ww.log_freq desc,LENGTH(ww.word) asc,ww.word asc";
+        $sql = "SELECT ww.word,ww.translation,ww.freq_level,ww.freq FROM wiki_word_rela wwr LEFT JOIN wiki_word ww ON wwr.similar_word_id=ww.id WHERE wwr.word_id=%d order by ww.log_freq desc,LENGTH(ww.word) asc,ww.word asc";
         return $this->query($sql,$wId);
     }
 	
